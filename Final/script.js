159c46ee044f7fef58b78dc35a1ebd77e7151e4b
jasonthorne <jasonthorne@live.ie>
2019-06-05 15:31:05 +0100

    adding onLoad

--- Final/script.js
+++ Final/script.js
@@ -3,30 +3,33 @@
 console.log("Yo");
 
 var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-console.log(canvas.width);
-//background vars: 
+var ctx = canvas.getContext("2d");
+
 var backgroundImg = new Image(); //background image
 backgroundImg.src = 'img/background.png'; //image source
-//var backgroundX  = //(window.innerWidth - canvas.width)/2; //x pos of background 
-//var backgroundY  = (window.innerHeight - canvas.height)/2;; //y pos of background 
-var backgroundW  = canvas.width; //width of background 
-var backgroundH  = canvas.height; //height of background 
 
 
-//var test1 = document.getElementById("canvasWrapper").getBoundingClientRect();
+//basket vars:
+var basketImg = new Image(); //basket image
+//var basketW = 100; //width of basket
+//var basketH = 100; //height of basket
+//var basketX = (canvas.width - basketW) /2; //x pos of basket 
+//var basketY = (canvas.height - basketH) - 10; //y pos of basket 
+
+var canvasBounds = canvas.getBoundingClientRect();
 
 
 //onload events:
 backgroundImg.onload = function(){
-    //context.drawImage(backgroundImg, test1.x, test1.y, test1.width, test1.height); //draw background
-     /*
+    ctx.drawImage(backgroundImg, canvasBounds.x, canvasBounds.y, canvasBounds.width, canvasBounds.height); //draw background
+
+
     //basket image source (assigned here so that background is drawn first)
-    basketImg.src = 'img/basket.png'; 
-   
+    basketImg.src = 'img/basket.png';
+
     //load and draw basket:
     basketImg.onload = function(){
-        context.drawImage(basketImg, basketX, basketY, basketW, basketH);    
-    }*/
+        ctx.drawImage(basketImg, 0, 0, basketImg.naturalWidth, basketImg.naturalHeight);   
+    }
     
 }
