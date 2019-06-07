671c62e92fae7c751cbbc7154e46137c441996d6
jasonthorne <jasonthorne@live.ie>
2019-06-07 13:02:39 +0100

    adding Basket2

--- Final/script.js
+++ Final/script.js
@@ -5,28 +5,38 @@ console.log("Yo");
 
 var canvas = document.getElementById("canvasId");
 var ctx = canvas.getContext("2d");
+//document.getElementById("timerId").innerHTML = 0; //set timer element //+++++++++++++++++++
+//document.getElementById("scoreId").innerHTML = 0; //set hits element //+++++++++++++++++++
 
 var backgroundImg = new Image(); //background image
 backgroundImg.src = 'img/background.png'; //image source
 var basketImg = new Image(); //basket image
+//var basketX; //x pos of basket 
+//var basketY; //y pos of basket 
+
+//var basketW = basketImg.width; //width of basket
+//var basketH = basketImg.height; //height of basket
+
+
+var key = []; //for keycode values 
+
+
+
 
 //----------------------------------------------------------------------------------------------------
 //onload events:
 backgroundImg.onload = function(){
-   
-    ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);  //   canvasBounds.width, canvasBounds.height); //draw background
+    
+    ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);  //draw background
        
     basketImg.src = 'img/basket.png'; //basket image source (assigned here so that background is drawn first)
     
     //load and draw basket:
     basketImg.onload = function(){
 
-        //ctx.drawImage(basketImg, (canvas.width - basketImg.width)/2, (canvas.height - basketImg.height) - 10, basketImg.width, basketImg.height);
-         ctx.drawImage(basketImg, ((canvas.width - basketImg.width)/2), ((canvas.height - basketImg.height) - 10), basketImg.width, basketImg.height);      
+         ctx.drawImage(basketImg, (canvas.width - basketImg.width)/2, (canvas.height - basketImg.height)-10, basketImg.width, basketImg.height); 
     }
     
-	//document.getElementById("timerId").innerHTML = 0; //set timer element
-    //document.getElementById("scoreId").innerHTML = 0; //set hits element
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -42,8 +52,8 @@ window.addEventListener("keyup", function(event){ //listens for key release, rem
 //----------------------------------------------------------------------------------------------------
 //create basket:
 var Basket = {
-    xPos: ((canvas.width - basketImg.width)/2),
-    yPos: ((canvas.height - basketImg.height) - 10),
+    xPos: (canvas.width - basketImg.width)/2, //x pos of basket ,
+    yPos: (canvas.height - basketImg.height)-10,
     width: basketImg.width, //maybe change this to a percentage of viewport +++++++++++++
     height: basketImg.height, //maybe change this to a percentage of viewport ++++++++++
     move: function(){
@@ -54,12 +64,15 @@ var Basket = {
                 Basket.xPos = 0; //stop at screen edge
             }
         }else if (key[39]){ //if right is pressed
-                Basket.xPos += 30; //increase x pos
-    
-                if(Basket.xPos > (canvas.width - Basket.width)){ //check for canvas collision
+            Basket.xPos += 30; //increase x pos
+           
+            if(Basket.xPos > (canvas.width - Basket.width)){ //check for canvas collision
                 Basket.xPos = canvas.width - Basket.width; //stop at screen edge
-                }
-        }      
+            }
+        }
+
+        //ctx.drawImage(basketImg, this.xPos, this.yPos, basketImg.width, basketImg.height);    
+        ctx.drawImage(basketImg, Basket.xPos, Basket.yPos, Basket.width, Basket.height);  
     }
 
 }
@@ -68,7 +81,18 @@ var Basket = {
 //create apple:
 
 
+
+//----------------------------------------------------------------------------------------------------
 //animate game:
 function animate(){
-    
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height); //clear canvas
+    ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);  //draw background
+    Basket.move(); //move basket
+
+    ///ctx.drawImage(basketImg, this.xPos, this.yPos, this.width, this.height);  
+
+    requestAnimationFrame(animate); //game continues
 }
+
+animate();
