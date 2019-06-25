7f5f07fbdd68dd995de376f9ddbf1e678af3746e
jasonthorne <jasonthorne@live.ie>
2019-06-25 14:49:59 +0100

    testing canvas layout

---
+++ Final/CanvasTest/canvasTestScript.js
@@ -0,0 +1,32 @@
+
+var backgroundCanvas = document.getElementById("background");
+var basketCanvas = document.getElementById("basket");
+
+var canvasW = backgroundCanvas.width; //canvas width
+var canvasH = backgroundCanvas.height; //canvas height
+
+var basketW = 110, basketH = basketW; //width & height of basket
+
+var backgroundCtx = backgroundCanvas.getContext("2d"); 
+var basketCtx = basketCanvas.getContext("2d"); 
+
+//------------
+
+
+var backgroundImg = new Image(); //background image
+backgroundImg.src = '../img/backgroundTEST6.png'; //image source
+
+var basketImg = new Image(); //basket image
+
+
+backgroundImg.onload = function(){
+    
+    backgroundCtx.drawImage(backgroundImg, 0, 0, canvasW, canvasH);  //draw background
+    basketImg.src = '../img/baskets/basket0.png'; //basket image source (assigned here so that background is drawn first)
+    
+   //load and draw basket:
+    basketImg.onload = function(){
+        basketCtx.drawImage(basketImg, (canvasW - basketW) /2, (canvasH - basketH) - 10, basketW, basketH); 
+    }
+    
+}
