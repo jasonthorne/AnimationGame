463a7e1ffd7b433e21cbef3be87e7a66bca0c1b2
jasonthorne <jasonthorne@live.ie>
2022-09-11 23:25:54 +0100

    module test

---
+++ A1/a.js
@@ -0,0 +1,13 @@
+
+import {hello} from "./b.js";
+
+const b = hello;
+
+/*console.log(b.text);
+console.log("hullo!");
+console.log(b.printTxt());*/
+
+console.log("dfdfdfedeee");
+
+//document.querySelector("#test").innerHTML = "dfdffdd";
+document.getElementById("test").innerHTML = b;
---
+++ A1/b.js
@@ -0,0 +1,16 @@
+
+/*
+export class B {
+	
+	constructor(){
+		this.text = "yo";
+	}
+	
+	printTxt(){
+		return this.text + " dawg";
+	}
+}
+*/
+
+export const hello = "Hullo there!";
+
